20fd32b0d5f0147dcacb7b61337aaecc1f7417e7
hhonghye <honghye194@gmail.com>
2021-08-21 22:52:59 +0900

    #visual

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
+/*----- DOM Caching -----*/
+
+
+
+/*----- function -----*/
+
+
+/*----- event -----*/
+
+
+/*----- function definition -----*/
+
+
+
+
+
+
+
 /* swiper연결 */
 var swiper = new Swiper('#visual .swiper', {
+    loop: true,
     pagination: {
-    el: '.swiper-pagination',
-    dynamicBullets: true,
+        el: '.swiper-pagination',
+        dynamicBullets: true,
     },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, //스와이프 후 자동재생
+    }
 });
