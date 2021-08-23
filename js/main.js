353816f2ca6076cdfb0fffb0be716f2368099a77
hhonghye <honghye194@gmail.com>
2021-08-23 19:56:17 +0900

    js, main-tab

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 /*----- DOM Caching -----*/
-
-
+var $main_tab__menu = $(".main-tab__menu").children("ul").children("li");
+var $main_tab__content = $(".main-tab__content").children("div");
+let isDone = true;
 
 /*----- function -----*/
 
 
 /*----- event -----*/
+$main_tab__menu.on("click", function(e){
+    e.preventDefault();
+
+    var hasTarget = $(this).hasClass("target");
+    if(hasTarget) return;
+
+    if(isDone){
+        isDone = false; 
+        var i = $(this).index();
+
+        mainTabActivation(i, $main_tab__menu);
+        mainTabActivation(i, $main_tab__content);
+    }
+
+})
 
 
 /*----- function definition -----*/
-
+function mainTabActivation(index, item){
+	item.removeClass("target"); 
+    item.eq(index).addClass("target"); 
+    isDone = true;
+}
 
 
 
