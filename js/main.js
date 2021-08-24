94b4fedad5daff96ddab2902321812d988b6d609
hhonghye <honghye194@gmail.com>
2021-08-25 00:31:05 +0900

    #coordi, autoplay icon 추가 & swiper autoplay 주석 해제

--- js/main.js
+++ js/main.js
@@ -171,10 +171,10 @@ var swiper = new Swiper('#visual', {
         dynamicBullets: true,
         clickable: true,
     },
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false, //스와이프 후 자동재생
-    // }
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, //스와이프 후 자동재생
+    }
 });
 var swiper = new Swiper('.sub-banner', {
     spaceBetween: 15,
@@ -184,8 +184,8 @@ var swiper = new Swiper('.sub-banner', {
       el: '.swiper-pagination',
       clickable: true,
     },
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false, //스와이프 후 자동재생
-    // }
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, //스와이프 후 자동재생
+    }
   });
